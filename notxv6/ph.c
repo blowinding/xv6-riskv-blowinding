e21f84bf2601dc1f8a951ab942ed4f1fb2682620
blowinding <2475258425@qq.com>
2024-08-11 11:04:00 +0800

    thread all

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -16,7 +16,7 @@ struct entry {
 struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
-
+pthread_mutex_t lock;
 
 double
 now()
@@ -47,6 +47,7 @@ void put(int key, int value)
     if (e->key == key)
       break;
   }
+  pthread_mutex_lock(&lock);
   if(e){
     // update the existing key.
     e->value = value;
@@ -54,6 +55,7 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
+  pthread_mutex_unlock(&lock);
 
 }
 
@@ -117,7 +119,7 @@ main(int argc, char *argv[])
   for (int i = 0; i < NKEYS; i++) {
     keys[i] = random();
   }
-
+  pthread_mutex_init(&lock, NULL);
   //
   // first the puts
   //
